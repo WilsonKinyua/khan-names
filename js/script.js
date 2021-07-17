1f9220ff8b0a79146a1a802a030cc0569b3fdc5e
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-07-17 21:22:21 +0300

    validating if gender is selected done

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 // variable from the form
-let inputDay;
-let inputMonth;
-let inputYear;
-let gender;
-let message;
+let inputDay, inputMonth, inputYear, gender, message;
+let akanMale = [];
+let akanFemale = [];
 
 // submit button
 const btnSubmit = document.querySelector(".submit");
 
 // adding event listener to the submit button
 btnSubmit.addEventListener("click", function () {
+
     // selecting elements
     inputDay = Number(document.getElementById("day").value);
     inputMonth = Number(document.getElementById("month").value);
     inputYear = Number(document.getElementById("year").value);
-    // gender = document.querySelector('input[name="gender"]:checked');
+    gender = document.querySelector('input[name="gender"]:checked');
     message = document.querySelector(".error");
-
+    
+   
     // validate date of birth user input
     if (inputDay <= 0 || inputDay > 31) {
         message.textContent = "Enter valid date";
@@ -27,10 +27,12 @@ btnSubmit.addEventListener("click", function () {
     } else if (inputYear <= 0 || inputYear > 9999) {
         message.textContent = "Enter a valid year";
         message.classList.remove("display");
-    }
-    // check if user year is before current year
+    }     // check if user year is before current year
     else if (inputYear > new Date().getFullYear()) {
         message.textContent = "Enter an year not greater than the current year 🙃";
+        message.classList.remove("display");
+    } else if(gender == null){
+        message.textContent = "Select Your gender";
         message.classList.remove("display");
     }else {
         message.classList.add("display");
