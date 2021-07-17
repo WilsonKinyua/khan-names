ec2b4e92639559af63ce9d18dae40499ae7874f2
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-07-18 00:24:44 +0300

    form validation completed

--- js/script.js
+++ js/script.js
@@ -2,24 +2,53 @@
 let inputDay, inputMonth, inputYear, gender, message, cc, yy;
 let akanMale = [];
 let akanFemale = [];
-let dayOfTheWeek;
+let daysOfTheWeek = [];
+// let test;
+// var dateOfBirth;
 // submit button
 const btnSubmit = document.querySelector(".submit");
 
 // adding event listener to the submit button
 btnSubmit.addEventListener("click", function () {
 
+    // test = document.querySelector("#test").value;
+    // console.log(test);
+    // test = new Date(test);
+    // console.log(test.getDay());
     // selecting elements
-    inputDay = Number(document.getElementById("day").value);
-    inputMonth = Number(document.getElementById("month").value);
-    inputYear = Number(document.getElementById("year").value);
+    inputDay = parseInt(document.getElementById("day").value);
+    inputMonth = parseInt(document.getElementById("month").value);
+    inputYear = parseInt(document.getElementById("year").value);
     gender = document.querySelector('input[name="gender"]:checked');
     message = document.querySelector(".error");
-    
-    // akanNames
-    akanMale = ["Kwasi","Kwadwo","Kwabena","Kwaku", "Yaw", "Kofi", "Kwame"];
-    akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
+    // akanNames
+    akanMale = [
+        "Kwasi",
+        "Kwadwo",
+        "Kwabena",
+        "Kwaku",
+        "Yaw",
+        "Kofi",
+        "Kwame"
+    ];
+    akanFemale = [
+        "Akosua",
+        "Adwoa",
+        "Abenaa",
+        "Akua",
+        "Yaa",
+        "Afua",
+        "Ama"];
+    daysOfTheWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
     // validate date of birth user input
     if (inputDay <= 0 || inputDay > 31) {
         message.textContent = "Enter valid date";
@@ -34,18 +63,22 @@ btnSubmit.addEventListener("click", function () {
     else if (inputYear > new Date().getFullYear()) {
         message.textContent = "Enter an year not greater than the current year 🙃";
         message.classList.remove("display");
-    } else if(gender == null){
+    } else if (gender == null) {
         message.textContent = "Select Your gender";
         message.classList.remove("display");
-    }else {
+    } else {
         message.classList.add("display");
         message.textContent = "";
 
-        cc = Number(inputYear.toString().substr(-2));
+        // cc = Number(inputYear.toString().substr(0, 2));
+        // yy = Number(inputYear.toString().substr(2,4));
+        // console.log("century is " + cc);
+        // console.log("Year is " + yy);
 
-        yy =  Number(inputYear.toString().slice(0, 2));
+        var monthBorn = parseInt(document.getElementById("month").value);
     
-     
+
+
     }
 
 });
